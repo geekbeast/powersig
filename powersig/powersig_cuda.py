de33b40a2d3630241ce733e27eede5b4d412a2b2
Matthew Tamayo <matthew@getmethodic.com>
2025-02-09 06:15:57 -0800

    Need to reduce resources consumed by kernel.

--- powersig/powersig_cuda.py
+++ powersig/powersig_cuda.py
@@ -19,7 +19,7 @@ def get_point(num_points, idx):
 
 
 @cuda.jit(device=True)
-def cuda_get_step_length(d: int, rows:int, cols:int) -> Tuple[int, int, int]:
+def cuda_get_step_length(d: int, rows: int, cols: int) -> Tuple[int, int, int]:
     if d < cols:
         # if d < cols, then we haven't hit the right edge of the grid
         t_start = 0
@@ -30,6 +30,7 @@ def cuda_get_step_length(d: int, rows:int, cols:int) -> Tuple[int, int, int]:
         s_start = cols - 1
 
     return s_start, t_start, min(rows - t_start, s_start + 1)
+
 
 def get_step_length(d: int, rows: int, cols: int) -> Tuple[int, int, int]:
     # d, s_start, t_start are 0 based indexes while rows/cols are shapes.
@@ -44,6 +45,7 @@ def get_step_length(d: int, rows: int, cols: int) -> Tuple[int, int, int]:
 
     return s_start, t_start, min(rows - t_start, s_start + 1)
 
+
 @cuda.jit
 def build_scaling_matrix(scaling_matrix: cuda.device_array, order: int = 32):
     tx = cuda.threadIdx.x
@@ -51,8 +53,9 @@ def build_scaling_matrix(scaling_matrix: cuda.device_array, order: int = 32):
     scaling_matrix[ty, tx] = 1.0 / ((ty + 1) * (tx + 1))
     cuda.syncthreads()
 
+
 @cuda.jit
-def compute_rho_diagonal(dX : cuda.device_array, dY : cuda.device_array, rho : cuda.device_array):
+def compute_rho_diagonal(dX: cuda.device_array, dY: cuda.device_array, rho: cuda.device_array):
     """
     CUDA kernel for computing the rho diagonal for the current step. Computes the dot product 
     starting at s_start, t_start and progressing up current_length steps from the bottom of the diagonal.
@@ -71,18 +74,18 @@ def compute_rho_diagonal(dX : cuda.device_array, dY : cuda.device_array, rho : c
     bx = cuda.blockIdx.x
     # We may not use all 32 entries if there aren't 32 threads for dimensions.
     shared_dp = cuda.shared.array(shape=(32,), dtype=np.float64)
-    
+
     if tx == 0:
         if ty == 0:
             rho[bx] = 0.0
         shared_dp[ty] = 0.0
-    
+
     cuda.syncthreads()
 
     # Using a local sum doesn't work since you end up with tx x ty registers that then need to be accumulated.
     local_sum = 0.0
     for i in range(0, d, step):
-        d_idx = i + tx + 32*ty 
+        d_idx = i + tx + 32 * ty
         if d_idx < d:
             local_sum += dX[bx, d_idx] * dY[bx, d_idx]
 
@@ -90,9 +93,10 @@ def compute_rho_diagonal(dX : cuda.device_array, dY : cuda.device_array, rho : c
     cuda.atomic.add(shared_dp, (ty), local_sum)
 
     cuda.syncthreads()
-    
-    if tx == 0:    
+
+    if tx == 0:
         cuda.atomic.add(rho, (bx), shared_dp[ty])
+
 
 @cuda.jit
 def compute_sigkernel_diagonal(N, M, step, global_scaling_matrix, rho_diagonal, input_diagonal, output_diagonal):
@@ -122,26 +126,26 @@ def compute_sigkernel_diagonal(N, M, step, global_scaling_matrix, rho_diagonal, 
 
     # Copy the scaling matrix to shared memory from global memory
     scaling_matrix[tx, ty] = global_scaling_matrix[tx, ty]
-   
+
     # u_0 = current boundary conditions computed in last step
     u[tx, ty] = input_diagonal[bx, tx, ty]
     u_n[0, tx, ty] = input_diagonal[bx, tx, ty]
-    
+
     # Need to compute propagation to other blocks
     # Only process if thread is within valid range for this step    
     s_start, t_start, step_length = cuda_get_step_length(step, N, M)
     _, _, next_step_length = cuda_get_step_length(step + 1, N, M)
-    
+
     # We need to compute the s, t indices for the current block
     # assuming the 0th block corresponds to the diagonal starting at s_start, t_start
     s_idx = s_start - bx
     t_idx = t_start + bx
-    
+
     s = get_point(N, s_idx)
     t = get_point(M, t_idx)
-    
+
     cuda.syncthreads()
-    
+
     # Scale for integration
     for i in range(31):
         # u_n = rho * double integral of u_{n-1} with correct limits
@@ -167,12 +171,12 @@ def compute_sigkernel_diagonal(N, M, step, global_scaling_matrix, rho_diagonal, 
             u_n[u_n_next_index, tx + 1, ty + 1] = u_n[u_n_current_index, tx, ty] * scaling_matrix[tx, ty]
 
             # Apply limits of integration using a warp aggregate
-            cuda.atomic.sub(u_n, (u_n_next_index, 0, ty+1), u_n[u_n_next_index, tx, ty+1] * t_ty)
+            cuda.atomic.sub(u_n, (u_n_next_index, 0, ty + 1), u_n[u_n_next_index, tx, ty + 1] * t_ty)
 
         # We've flipped indexing here to take advantage of warp aggregation
         if tx < 31:
             # We have to subtract out this portion of the semi-definite integral. This will actually perform an addition into (0,0)
-            cuda.atomic.sub(u_n, (u_n_next_index, ty, 0), u_n[u_n_next_index, ty, tx +1] * s_tx)
+            cuda.atomic.sub(u_n, (u_n_next_index, ty, 0), u_n[u_n_next_index, ty, tx + 1] * s_tx)
 
         cuda.syncthreads()
 
@@ -184,44 +188,62 @@ def compute_sigkernel_diagonal(N, M, step, global_scaling_matrix, rho_diagonal, 
 
     # Write solution of block in input diagonal to grid. This can be skipped since we already have it in shared memory
     # but its useful for debugging to make sure right values are being computed at each step.
+
+
+    # If the length of the next diagonal is the same or shorter than the current diagonal
+    # then all blocks propagate to the right. Otherwise, skip propagating the first one and reduce first dimension by 1.
+
+    # If the length of the next diagonal is longer than the current diagonal
+    # then all blocks propagate up. Otherwise, skip propagating the last one, but do not adjust first dimension.
+
+    bottom_offset = 0
+    top_limit = step_length
+
+    if step_length > next_step_length:
+        bottom_offset = 1  # skip propagating first bottom
+        top_limit = step_length - 1  # skip propagating last top
+    elif step_length == next_step_length:
+        bottom_offset = 0  # propagate first bottom.
+        top_limit = step_length - 1  # skip propagating last top
+
+    # Next steps.
+    s = get_point(N, s_idx + 1)
+    t = get_point(M, t_idx + 1)
+    s_coeff = (s ** tx)
+    t_coeff = (t ** ty)
+
+    # Only propagate to the right if we are not at the right limit. Should result in warp aggregate for each chunk of 32 threads.
+    if bx > bottom_offset:
+        if tx == 0:
+            u_n[0, 0, ty] = 0.0
+        # Propagate the right boundary condition resulting in a function of s
+        cuda.atomic.add(u_n, (0, 0, ty), u[tx, ty] * t_coeff)
+
+        # Always use this to compute initial condition, if you are in a valid block
+        cuda.syncthreads()
+        cuda.atomic.add(u_n, (0, 0, 0), u_n[0,0, ty] * s_coeff)
+
+    # Only propagate to the top if we are not at the top limit
+    if bx < top_limit:
+        if tx == 0:
+            u_n[0, ty, 0] = 0.0
+        # Propagate to the top boundary condition resulting in a function of t.
+        # Reverse tx and ty, so that we get warp aggregate for each chunk of 32 threads.
+        cuda.atomic.add(u_n, (0, ty, 0), u[ty, tx] * s_coeff)
+
+        # Only use this to compute initial condition if it hasn't already been computed.
+        if bx <= bottom_offset:
+            cuda.syncthreads()
+            cuda.atomic.add(u_n, (0, 0, 0), u_n[0, ty, 0] * t_coeff)
+
+
+    if bx < top_limit or bx > bottom_offset:
+        if tx == 0:
+            output_diagonal[bx, 0, ty] = u_n[0, 0, ty]
+        elif tx == 1:
+            output_diagonal[bx, ty, 0] = u_n[0, ty, 0]
+
     input_diagonal[bx, tx, ty] = u[tx, ty]
-    
-    # # If the length of the next diagonal is the same or shorter than the current diagonal
-    # # then all blocks propagate to the right. Otherwise, skip propagating the first one and reduce first dimension by 1.
-    
-    # # If the length of the next diagonal is longer than the current diagonal
-    # # then all blocks propagate up. Otherwise, skip propagating the last one, but do not adjust first dimension.
-    
-    # bottom_offset = 0
-    # top_limit = step_length
-
-    # if step_length > next_step_length:
-    #     bottom_offset = 1  # skip propagating first bottom
-    #     top_limit = step_length - 1  # skip propagating last top
-    # elif step_length == next_step_length:
-    #     bottom_offset = 0  # propagate first bottom.
-    #     top_limit = step_length - 1  # skip propagating last top
-
-    # # Next steps.
-    # s_right = get_point(N, s_idx + 1)
-    # t_above = get_point(M, t_idx + 1)
-    # s_coeff = (s_right ** tx)
-    # t_coeff = (t_above ** ty)
-    
-    # # Only propagate to the right if we are not at the right limit. Should result in warp aggregate for each chunk of 32 threads.
-    # if bx > bottom_offset and tx > 0:
-    #     resolved_entry = input_diagonal[bx - bottom_offset, tx, ty] * t_coeff
-    #     # Propagate the right boundary condition resulting in a function of s
-    #     cuda.atomic.add(output_diagonal, (bx, 0, ty),resolved_entry)
-    #     # Propagate the initial condition
-    #     cuda.atomic.add(output_diagonal, (bx, 0, 0),resolved_entry * s_coeff)
-    
-    # # Only propagate to the top if we are not at the top limit
-    # if bx < top_limit and ty > 0:
-    #     # Propagate to the top boundary condition resulting in a function of t. 
-    #     # Reverse tx and ty, so that we get warp aggregate for each chunk of 32 threads.
-    #     cuda.atomic.add(output_diagonal, (bx, ty, 0),input_diagonal[bx, ty, tx] * s_coeff)
-
 
 def launch_scaling_matrix(order: int = 32):
     assert order <= 32, "Order must be less than or equal to 32"
@@ -230,6 +252,7 @@ def launch_scaling_matrix(order: int = 32):
     scaling_matrix = cuda.device_array((order, order), dtype=np.float64)
     build_scaling_matrix[blockspergrid, threadsperblock](scaling_matrix, order)
     return scaling_matrix
+
 
 # Host function to set up and launch kernel
 def compute_signature(dX, dY) -> float:
@@ -255,31 +278,32 @@ def compute_signature(dX, dY) -> float:
     dY = cuda.to_device(dY)
 
     min_NM = min(N, M)
-    
+
     total_diagonals = N + M - 1
 
     # Function to compute result length for each step
 
     # Total steps
-    total_diagonals = N + M - 1    
+    total_diagonals = N + M - 1
 
     # Calculate grid and block dimensions
     threadsperblock = (32, 32)
-    rho_threadsperblock = (32, get_number_threads(dX.shape[1]//32))
+    rho_threadsperblock = (32, get_number_threads(dX.shape[1] // 32))
     start = time.time()
-    
+
     scaling_matrix = launch_scaling_matrix()
-    
+
     s_start, t_start, current_length = get_step_length(0, N, M)
     s_next, t_next, next_length = get_step_length(1, N, M)
 
     # Initialize input and output diagonals on device
     input_diagonal = cuda.device_array((min_NM, 32, 32), dtype=np.float64)
-    output_diagonal = cuda.device_array((min_NM, 32, 32), dtype=np.float64)    
+    output_diagonal = cuda.device_array((min_NM, 32, 32), dtype=np.float64)
     rho_diagonal = cuda.device_array((min_NM,), dtype=np.float64)
-    input_diagonal[0,0,0] = 1
-    for i in range(total_diagonals):        
-        print(f"Processing {i}th diagonal with s_start={s_start}, t_start={t_start}, current_length={current_length}, next_length={next_length}")
+    input_diagonal[0, 0, 0] = 1
+    for i in range(total_diagonals):
+        print(
+            f"Processing {i}th diagonal with s_start={s_start}, t_start={t_start}, current_length={current_length}, next_length={next_length}")
         blockspergrid = (current_length)
 
         print(f"Blocks per grid: {blockspergrid}")
@@ -289,10 +313,10 @@ def compute_signature(dX, dY) -> float:
         dstart = time.time()
         # Compute the rho diagonal for the current step
         compute_rho_diagonal[blockspergrid, rho_threadsperblock](
-            dX[s_start:(s_start+current_length), :], 
-            dY[t_start:(t_start+current_length), :], 
+            dX[s_start:(s_start + current_length), :],
+            dY[t_start:(t_start + current_length), :],
             rho_diagonal[:current_length]
-            )
+        )
         cuda.synchronize()
         print(f"Compute rho diagonal in {(time.time() - dstart)} seconds: {rho_diagonal.copy_to_host()}")
 
@@ -301,14 +325,14 @@ def compute_signature(dX, dY) -> float:
         print(f"(Before) Input diagonal: {input_diagonal[:current_length, :, :].copy_to_host()}")
         dstart = time.time()
         # Compute the sigkernel diagonal
-        compute_sigkernel_diagonal[blockspergrid,threadsperblock](
+        compute_sigkernel_diagonal[blockspergrid, threadsperblock](
             N,
             M,
             i,
             scaling_matrix,
             rho_diagonal[:current_length],
-            input_diagonal[:current_length,:,:],
-            output_diagonal[:next_length,:,:]
+            input_diagonal[:current_length, :, :],
+            output_diagonal[:next_length, :, :]
         )
 
         print(f"Kernel launched for {i}th diagonal in {(time.time() - dstart)} seconds")
@@ -324,7 +348,6 @@ def compute_signature(dX, dY) -> float:
         input_diagonal, output_diagonal = output_diagonal, input_diagonal
 
     print(f"Processed {dX.shape[0]}x{dY.shape[0]} grid in {(time.time() - start)} seconds")
-    
 
     return output_diagonal.copy_to_host().sum()
 
@@ -333,11 +356,9 @@ def compute_signature(dX, dY) -> float:
 if __name__ == "__main__":
     # dX = np.random.random((1 << 2, 2)).astype(np.float64)
     # dY = np.random.random((1 << 2, 2)).astype(np.float64)
-    dX = np.asarray([[2,2,2,2]]).astype(np.float64)
-    dY = np.asarray([[2,2,2,2]]).astype(np.float64)
-    
+    dX = np.asarray([[2, 2, 2, 2]]).astype(np.float64)
+    dY = np.asarray([[2, 2, 2, 2]]).astype(np.float64)
 
     # Process entire grid
 
     result = compute_signature(dX, dY)
-
